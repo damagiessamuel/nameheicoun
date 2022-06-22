34f0113625cc4bb7671bc0b0b810ed5c1bf8b42b
Chukwunonso Samuel Enuoyibo <105329788+damagiessamuel@users.noreply.github.com>
2022-06-22 23:10:17 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+window.alert("NAME: Chukwunonso Samuel Enuoyibo.                                                HEIGHT: 1.87m                                                                  COUNTRY:Nigeria");
+	
